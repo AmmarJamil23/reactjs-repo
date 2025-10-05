4fc7c8e07f37a23c552bae5cc3dce89480792ebb
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-05 13:47:49 +0500

    made reusable cards and also learned props

---
+++ reactbasic/src/components/UserCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './UserCard.css'
+
+const UserCard = (props) => {
+  return (
+    <div className='user-container'>
+        <p id='user-name'>{props.name}</p>
+        <img id='user-image' src={props.image} alt="user" />
+        <p id='user-desc'>{props.desc}</p>
+
+    </div>
+  )
+}
+
+export default UserCard
