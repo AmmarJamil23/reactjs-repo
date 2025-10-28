0966dfd3bd459caad7c5aba8e5df31b1b477b3b6
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-28 21:54:58 +0500

    learnt basic components created two of them

---
+++ components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1>Heading</h1>
+        <p>Lorem ipsum, dolor sit 
+            amet consectetur 
+            adipisicing elit. 
+            Necessitatibus odio 
+            earum, minima nobis, cum temporibus maiores debitis tenetur dolores sit, laudantium in molestias aperiam. Modi suscipit architecto sequi porro necessitatibus.</p>
+    </div>
+  )
+}
+
+export default Card
