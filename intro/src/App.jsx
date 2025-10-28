f28828c132942e0cc4f05d160c364359795aa86d
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-28 12:55:26 +0500

    created strcuture

---
+++ intro/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>User1</h1>
+      <h2>User 2</h2>
+      <h3>3rd heading</h3>
+    </div>
+  )
+}
+
+export default App
