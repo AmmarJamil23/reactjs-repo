ecf4ebeff8f4e06efe4103407f3fe5df4877bff6
AmmarJamil23 <ammarjamil7000@gmail.com>
2025-10-30 21:45:00 +0500

    map, foreach

--- 4-cards-project/src/App.jsx
+++ 4-cards-project/src/App.jsx
@@ -3,13 +3,34 @@ import { Bookmark } from 'lucide-react'
 import Card from './components/Card'
 
 const App = () => {
+  // const arr = [
+  //   {
+  //     user: 'AJ',
+  //     age: 20
+  //   }, 
+  //   {
+  //     user: 'DJ',
+  //     age: 25
+  //   }, 
+  //   {
+  //     user: "AG",
+  //     age: 18
+  //   }
+  // ];
+
+  // arr.forEach((elem) => {
+  //   console.log(elem.user)
+  // })
+  
   return (
     <div className='parent'>
+      
 
+   
+    {/* <Card />
     <Card />
     <Card />
-    <Card />
-    <Card />
+    <Card /> */}
 
 
     </div>
